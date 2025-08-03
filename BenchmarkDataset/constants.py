d085c7abec429a6c5659aef94083390eec9bcef0
Coco Zeng <64930215+superasymmetry@users.noreply.github.com>
2025-08-03 11:13:28 -0400

    Create constants.py

---
+++ BenchmarkDataset/constants.py
@@ -0,0 +1,21 @@
+# Concepts in each domain
+literature = ["Haiku", "Shakespearean Sonnet", "Analogy", "Paradox", "Anacoluthon", "Asyndeton", "Hyperbaton", "Synesis", "Accismus", "Slant Rhyme", "Enthymeme", "Anapest"]
+psychological_biases = ["Fundamental Attribution Error", "Demanding Bias", "Black and White Thinking", "Sunk Cost Fallacy", "IKEA Effect", "Pseudocertainty Effect", "Endowment Effect", "Naive Cynicism", "Normalcy Bias", "Spotlight Effect", "Illusory Superiority", "Catastrophizing"]
+game_theory = ["Strict Dominance", "Iterated Dominance", "Weak Dominance", "Pure Nash Equilibrium", "Mixed Strategy Nash Equilibrium", "Pareto Optimality", "Best Response", "Zero-Sum Game", "Symmetric Game"]
+
+
+# Models and corresponding short names
+models_to_short_name = {
+    "meta-llama/Llama-3.3-70B-Instruct-Turbo": "Llama-3.3",
+    "gpt-4o": "GPT-4o",
+    "gpt-4.5-preview": "GPT-4.5",
+    "o3-mini": "O3-Mini",
+    "o1-mini": "O1-Mini",
+    "gemini-2.0-flash-exp": "Gemini-2.0",
+    "claude-3-5-sonnet-20241022": "Claude-Sonnet",
+    "mistralai/Mistral-7B-Instruct-v0.2": "Mistral-Instruct",
+    "deepseek-ai/DeepSeek-V3": "DeepSeek-V3",
+    "deepseek-ai/DeepSeek-R1": "DeepSeek-R1",
+    "Qwen/Qwen2-VL-72B-Instruct": "Qwen2-VL"
+}
+models = list(models_to_short_name.keys())
